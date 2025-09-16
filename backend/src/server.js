6989dced5a64b01b5f17016768961f834471df1f
Abhay Gupta <abhaygupta9397@gmail.com>
2025-09-16 20:18:32 +0530

    side bar is updated successfully

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,7 +12,7 @@ const PORT = ENV.PORT || 3000;
 
 
 const app = express();
-app.use(express.json()) //req.body
+app.use(express.json({limit: "5mb"})); //req.body
 app.use(cors({origin: ENV.CLIENT_URL, credentials:true}));
 app.use(cookieParser());
 const __dirname = path.resolve();
